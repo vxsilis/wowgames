d9147ac72ec069576659a0b360a88b013b44a3b2
vxsilis <56308691+vxsilis@users.noreply.github.com>
2023-05-15 17:30:20 +0300

    Update script.js

--- memory/public/script.js
+++ memory/public/script.js
@@ -26,7 +26,7 @@ let rc = params.rc;
 if (rc == null) {// if there is no code passed as parameter go to homepage
     $('.waiting-message').html("looks like you shouldn't be here.. you will be redirected to homepage");
     setTimeout(function() {
-        window.location.href = "https://wowgames.gr/type.html";
+        window.location.href = "https://wowgames.gr/";
     }, 3000);
 }
 
